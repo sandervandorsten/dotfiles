f2130880263def420bfead1c47ea64700e1981c3
Sander van Dorsten <sander.vandorsten@digital-power.com>
2023-07-17 16:45:24 +0200

    added installation scripts

---
+++ .dotfiles_scripts/create_ssh_key.sh
@@ -0,0 +1,5 @@
+# create SSH key
+ssh-keygen -t ed25519 -C "sandervandorsten@gmail.com"
+
+echo "Please add this SSH Key to github. If you need more SSH Keys, add 
+them manually as well and add to representative system."
